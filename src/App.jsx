e8e54698b144729da047ec6da0603ce93911c6c4
Farzana Reefat Raha <reefat.raha2018@gmail.com>
2024-03-10 04:54:49 +0600

    Added Open Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import NavBar from './components/NavBar';
+import OpenSidebar from './components/OpenSidebar';
 
 
 function App() {
   return (
     <>
       <NavBar />
+      <OpenSidebar />
     </>
   )
 }
