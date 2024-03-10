88df739c2682efea2a797db18b4dbe3f17f0f168
Farzana Reefat Raha <reefat.raha2018@gmail.com>
2024-03-10 19:35:15 +0600

    Added responsive sidebar

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import OpenSidebar from '../components/OpenSidebar'
 import ClosedSidebar from '../components/ClosedSidebar'
 
@@ -7,6 +7,17 @@ const Sidebar = () => {
     const toggleSidebar = () => {
         setIsOpen(!isOpen);
     }
+    useEffect(() => {
+        const closeSidebarOnOutsideClick = (event) => {
+            if (isOpen && !event.target.closest('.menu')) {
+                setIsOpen(false);
+            }
+        };
+        document.body.addEventListener('click', closeSidebarOnOutsideClick);
+        return () => {
+            document.body.removeEventListener('click', closeSidebarOnOutsideClick);
+        };
+    }, [isOpen]);
     return (
         <div>
             {isOpen ? (
