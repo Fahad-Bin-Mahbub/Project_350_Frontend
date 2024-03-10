a12a5faeac6f466e42f0862b25688e753bdf66cf
Fahad Bin Mahbub <fahadbm3234@gmail.com>
2024-03-11 02:10:19 +0600

    react icons

--- src/pages/Exam.jsx
+++ src/pages/Exam.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SlidePane from "../components/SlidePane";
 import { Icons } from "../assets/icons";
-
+import { PiExamFill } from "react-icons/pi";
 const Exam = () => {
 	return (
 		<>
 			<img src={Icons.icon_exam} alt="" />
+			<PiExamFill />
 			<SlidePane />
 		</>
 	);
