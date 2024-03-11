42c754d74ce621cf8371e767e8cb2234fec2480d
Rabbani2019331039 <fathoor39@student.sust.edu>
2024-03-11 10:05:26 +0600

    card section component completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,52 @@ import NavBar from "./components/NavBar";
 import OpenSidebar from "./components/OpenSidebar";
 import TaskCard from "./components/TaskCard";
 
+const taskCardData = [
+	{
+		status: "delivered",
+		courseCode: "CSE461",
+		semester: "3/2",
+		teacher: "AT",
+	},
+	{
+		status: "in progress",
+		courseCode: "CSE450",
+		semester: "4/2",
+		teacher: "FC",
+	},
+	{
+		status: "delivered",
+		courseCode: "CSE460",
+		semester: "2/2",
+		teacher: "IZ",
+	},
+	{
+		status: "checked",
+		courseCode: "CSE103",
+		semester: "1/1",
+		teacher: "EH",
+	},
+	{
+		status: "accepted",
+		courseCode: "PHY203",
+		semester: "2/2",
+		teacher: "SC",
+	},
+];
+
 function App() {
 	return (
 		<>
 			{/* <NavBar /> */}
 			{/* <OpenSidebar /> */}
 			{/* <ClosedSidebar /> */}
-			<CardSection>
-				<TaskCard
+			<CardSection TaskCardsData={taskCardData} SectionName={"All cards"} />
+			{/* <TaskCard
 					status={"delivered"}
 					courseCode={"CSE461"}
 					semester={"3/2"}
 					teacher={"AT"}
-				/>
-			</CardSection>
+				/> */}
 		</>
 	);
 }
--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -1,13 +1,22 @@
-import { FaEllipsis } from "react-icons/fa6";
+import { FaEllipsis, FaPlus } from "react-icons/fa6";
+import TaskCard from "./TaskCard";
 
-const CardSection = () => {
+const CardSection = ({ SectionName, TaskCardsData }) => {
+	SectionName = SectionName.toUpperCase();
 	return (
-		<div className="w-360 h-840 bg-gray-100 rounded-xl flex flex-col p-6">
+		<div className="w-360 h-840 bg-gray-100 rounded-xl flex flex-col p-6 ">
 			<div className="flex justify-between pb-6">
-				<div className="text-sm text-black">Section Name</div>
-				<div>
+				<div className="text-sm text-black">{SectionName}</div>
+				<div className="flex">
+					{/* //TODO: Toggle the visibility of the plus icon according to the role */}
+					<FaPlus className="mx-2" />
 					<FaEllipsis />
 				</div>
+			</div>
+			<div className="overflow-x-hidden no-scrollbar">
+				{TaskCardsData.map((taskCardData) => (
+					<TaskCard {...taskCardData} />
+				))}
 			</div>
 		</div>
 	);
