d79cff4fc81b07c9c200bb2e9542dc670f522e2d
Fahad Bin Mahbub <fahadbm3234@gmail.com>
2024-03-20 02:21:14 +0600

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import './App.css'
-import NavBar from './components/NavBar';
-import OpenSidebar from './components/OpenSidebar';
-import ClosedSidebar from './components/ClosedSidebar';
-import Sidebar from './pages/Sidebar';
-import Exam from './pages/Exam';
-
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import OpenSidebar from "./components/OpenSidebar";
+import ClosedSidebar from "./components/ClosedSidebar";
+import Sidebar from "./pages/Sidebar";
+import Exam from "./pages/Exam";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      {/* <NavBar />
+	return (
+		<>
+			<Routes>
+				{/* <NavBar />
       <Sidebar /> */}
-      <Exam />
-    </>
-  )
+				{/* <Exam /> */}
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</>
+	);
 }
 
-export default App
+export default App;
