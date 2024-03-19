9de3c7020d9bbd83eccb82630b6b4a9151ab40ce
Farzana Reefat Raha <reefat.raha2018@gmail.com>
2024-03-20 03:55:33 +0600

    Started Admin Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Sidebar from './components/Sidebar';
+import AdminDashboard from './pages/Admin/AdminDashboard';
 
 function App() {
   return (
     <>
-      <Sidebar></Sidebar>
+      {/* <Sidebar></Sidebar> */}
+      <AdminDashboard></AdminDashboard>
     </>
   )
 }
