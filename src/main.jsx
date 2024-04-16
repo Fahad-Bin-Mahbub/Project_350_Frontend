a726f316210fc7cfaec0b00abdf108072a2c0690
Fahad Bin Mahbub <fahadbm3234@gmail.com>
2024-04-16 15:32:30 +0600

    test

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,8 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
     <AuthProvider>
       <App />
       <ToastContainer />
     </AuthProvider>
-  </BrowserRouter>
 );
