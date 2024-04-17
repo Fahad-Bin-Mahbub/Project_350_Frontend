ed70a12d47a1be56a8b0342eab7890c5c36a786d
Md.Shibly Rahman Alve <alverohman2000@gmail.com>
2024-04-17 12:56:45 +0600

    fixed context

--- src/App.jsx
+++ src/App.jsx
@@ -11,59 +11,68 @@ import LoginPage from "./pages/LoginPage";
 import TaskAssignPage from "./pages/TaskAssignPage";
 
 function App() {
-	return (
-		<>
-			<Router>
-				<Routes>
-					<Route path="/" element={<LoginPage />} />
-					<Route path="/admin/dashboard" element={<AdminDashboard />} />
-					<Route path="/invitationAccept" element={<InvitationAcceptPage />} />
-					<Route
-						path="/dashboard"
-						element={
-							<SectionProvider>
-								<TaskCardProvider>
-									<DashboardPage />
-									{/* <AdminDashboard /> */}
-								</TaskCardProvider>
-							</SectionProvider>
-						}
-					/>
-					<Route
-						path="/manage-ci"
-						element={
-							<SectionProvider>
-								<TaskCardProvider>
-									<CiManagementPage />
-								</TaskCardProvider>
-							</SectionProvider>
-						}
-					/>
-					<Route
-						path="/assign-task"
-						element={
-							<SectionProvider>
-								<TaskCardProvider>
-									<TaskAssignPage />
-								</TaskCardProvider>
-							</SectionProvider>
-						}
-					/>
-				</Routes>
-			</Router>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/admin/dashboard"
+            element={
+              <SectionProvider>
+                <TaskCardProvider>
+                  <AdminDashboard />
+                </TaskCardProvider>
+              </SectionProvider>
+            }
+          />
+          <Route path="/invitationAccept" element={<InvitationAcceptPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <SectionProvider>
+                <TaskCardProvider>
+                  <DashboardPage />
+                  {/* <AdminDashboard /> */}
+                </TaskCardProvider>
+              </SectionProvider>
+            }
+          />
+          <Route
+            path="/manage-ci"
+            element={
+              <SectionProvider>
+                <TaskCardProvider>
+                  <CiManagementPage />
+                </TaskCardProvider>
+              </SectionProvider>
+            }
+          />
+          <Route
+            path="/assign-task"
+            element={
+              <SectionProvider>
+                <TaskCardProvider>
+                  <TaskAssignPage />
+                </TaskCardProvider>
+              </SectionProvider>
+            }
+          />
+        </Routes>
+      </Router>
 
-			{/* <TaskCardProvider> */}
-			{/* <NavBar /> */}
-			{/* <OpenSidebar /> */}
-			{/* <ClosedSidebar /> */}
-			{/* <Sidebar /> */}
-			{/* <CardSection TaskCardsData={taskCardData} SectionName={"All cards"} /> */}
-			{/* <DashboardPage /> */}
-			{/* <AdminDashboard /> */}
-			{/* <LoginPage /> */}
-			{/* </TaskCardProvider> */}
-		</>
-	);
+      {/* <TaskCardProvider> */}
+      {/* <NavBar /> */}
+      {/* <OpenSidebar /> */}
+      {/* <ClosedSidebar /> */}
+      {/* <Sidebar /> */}
+      {/* <CardSection TaskCardsData={taskCardData} SectionName={"All cards"} /> */}
+      {/* <DashboardPage /> */}
+      {/* <AdminDashboard /> */}
+      {/* <LoginPage /> */}
+      {/* </TaskCardProvider> */}
+    </>
+  );
 }
 
 export default App;
