3e8d3667ed3220bebf16defc8b310b1dcbe8d175
Fahad Bin Mahbub <fahadbm3234@gmail.com>
2024-04-18 17:24:49 +0600

    task data

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -9,33 +9,22 @@ import { useAuth } from "../context/Auth.jsx";
 import { useSection } from "../context/SectionProvider.jsx";
 
 const DashboardPage = () => {
-  const [isOpenPane, setIsOpenPane] = useState(false);
-  const { sectionData, updateSectionData } = useSection();
-  const [all, setAll] = useState([]);
-  const [taskCardData1, setTaskCardData1] = useState([]);
-  const [taskCardData2, setTaskCardData2] = useState([]);
-  const [taskCardData3, setTaskCardData3] = useState([]);
-  const [taskCardData4, setTaskCardData4] = useState([]);
-  const [auth] = useAuth();
-  const baseUrl = "https://examtrack.up.railway.app";
-  const sections = ["1st Year", "2nd Year", "3rd Year", "4th Year"];
+	const [isOpenPane, setIsOpenPane] = useState(false);
+	const { sectionData, updateSectionData } = useSection();
+	const [all, setAll] = useState([]);
+	const [taskCardData1, setTaskCardData1] = useState([]);
+	const [taskCardData2, setTaskCardData2] = useState([]);
+	const [taskCardData3, setTaskCardData3] = useState([]);
+	const [taskCardData4, setTaskCardData4] = useState([]);
+	const [auth] = useAuth();
+	const baseUrl = "https://examtrack.up.railway.app";
+	const sections = ["1st Year", "2nd Year", "3rd Year", "4th Year"];
 
-  useEffect(() => {
-    updateSectionData([]);
-    console.log(JSON.stringify(auth));
+	useEffect(() => {
+		updateSectionData([]);
+		console.log(JSON.stringify(auth));
 
-    const options = {
-      method: "GET",
-      url: `${baseUrl}/api/task/get-teacher-tasks`,
-      withCredentials: true,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-      },
-    };
-    console.log(options);
-
-    axios
+		axios
 			.get(`${baseUrl}/api/task/get-teacher-tasks`, { withCredentials: true })
 			.then((response) => {
 				const { status, data } = response;
@@ -50,8 +39,8 @@ const DashboardPage = () => {
 							teacher: `${item.teacher.firstName} ${item.teacher.lastName}`,
 							dueDate: item.dueDate.split("T")[0],
 						};
-            console.log(taskCardData)
-
+						console.log(taskCardData);
+						console.log(item.year);
 						switch (item.year) {
 							case 1:
 								setTaskCardData1((prevData) => [...prevData, taskCardData]);
@@ -62,89 +51,70 @@ const DashboardPage = () => {
 							case 3:
 								setTaskCardData3((prevData) => [...prevData, taskCardData]);
 								break;
-							default:
+							case 4:
 								setTaskCardData4((prevData) => [...prevData, taskCardData]);
+								break;
+							default:
+								break;
 						}
-            console.log("TaskCardData1", taskCardData1)
-            console.log("TaskCardData2", taskCardData2)
-            console.log("TaskCardData3", taskCardData3)
-            console.log("TaskCardData4", taskCardData4)
+						console.log("TaskCardData1", taskCardData1);
+						console.log("TaskCardData2", taskCardData2);
+						console.log("TaskCardData3", taskCardData3);
+						console.log("TaskCardData4", taskCardData4);
 					});
 				}
 			})
 			.catch((error) => {
 				console.error("Error fetching teacher tasks:", error);
 			});
+	}, []);
 
-  }, []);
-
-  return (
-    <>
-      <div className="bg-blue-950 ">
-        <Sidebar>
-          <div className=" bg-white rounded-s-badge h-screen min-h-screen overflow-hidden">
-            <NavBar />
-            <div className=" flex px-3 w-full overflow-x-auto no-scrollbar">
-              <Drawer
-                open={isOpenPane}
-                onClose={() => setIsOpenPane(false)}
-                size="md"
-              >
-                <SlidePane />
-              </Drawer>
-              {sections.map((section, index) => {
-                if (index == 0) {
-                  <CardSection
-                    key={section}
-                    SectionName={section}
-                    TaskCardsData={taskCardData1}
-                    clickHandler={() => {
-                      setIsOpenPane(true);
-                    }}
-                  />;
-                } else if (index == 1) {
-                  <CardSection
-                    key={section}
-                    SectionName={section}
-                    TaskCardsData={taskCardData2}
-                    clickHandler={() => {
-                      setIsOpenPane(true);
-                    }}
-                  />;
-                } else if (index == 2) {
-                  <CardSection
-                    key={section}
-                    SectionName={section}
-                    TaskCardsData={taskCardData3}
-                    clickHandler={() => {
-                      setIsOpenPane(true);
-                    }}
-                  />;
-                } else {
-                  <CardSection
-                    key={section}
-                    SectionName={section}
-                    TaskCardsData={taskCardData4}
-                    clickHandler={() => {
-                      setIsOpenPane(true);
-                    }}
-                  />;
-                }
-              })}
-              {sections.length == 0 && (
-                <div className="flex items-center justify-center content-center m-auto ">
-                  <div className="text-2xl text-black">
-                    Select Session to view tasks
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
-        </Sidebar>
-      </div>
-      <footer></footer>
-    </>
-  );
+	return (
+		<>
+			<div className="bg-blue-950 ">
+				<Sidebar>
+					<div className=" bg-white rounded-s-badge h-screen min-h-screen overflow-hidden">
+						<NavBar />
+						<div className=" flex px-3 w-full overflow-x-auto no-scrollbar">
+							<Drawer
+								open={isOpenPane}
+								onClose={() => setIsOpenPane(false)}
+								size="md"
+							>
+								<SlidePane />
+							</Drawer>
+							{sections.map((section, index) => (
+								<CardSection
+									key={section}
+									SectionName={section}
+									TaskCardsData={
+										index === 0
+											? taskCardData1
+											: index === 1
+											? taskCardData2
+											: index === 2
+											? taskCardData3
+											: taskCardData4
+									}
+									clickHandler={() => {
+										setIsOpenPane(true);
+									}}
+								/>
+							))}
+							{sections.length === 0 && (
+								<div className="flex items-center justify-center content-center m-auto ">
+									<div className="text-2xl text-black">
+										Select Session to view tasks
+									</div>
+								</div>
+							)}
+						</div>
+					</div>
+				</Sidebar>
+			</div>
+			<footer></footer>
+		</>
+	);
 };
 
 export default DashboardPage;
