f501377f83597de6abdbb52f9c134c7773a610a7
Md.Shibly Rahman Alve <alverohman2000@gmail.com>
2024-04-18 08:41:50 +0600

    saved auth

--- src/components/Helper.jsx
+++ src/components/Helper.jsx
@@ -1,26 +1,34 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { useAuth } from "../context/Auth";
 
 const Helper = () => {
-	const { id } = useParams();
-	console.log(id);
-	const baseUrl = "https://examtrack.up.railway.app";
-	const navigate = useNavigate();
-	useEffect(() => {
-		//call api to fetch user   info using this id and navigate accordingly.
-		const options = {
-			method: "GET",
-			url: `${baseUrl}/api/user/get-user/${id}`,
-		};
+  const { id } = useParams();
+  console.log(id);
+  const baseUrl = "https://examtrack.up.railway.app";
+  const navigate = useNavigate();
+  const [auth, setAuth] = useAuth();
 
-		axios.request(options).then((response) => {
-			const { status } = response;
-			if (status == 200) {
-				navigate("/dashboard");
-			}
-		});
-	}, []);
+  useEffect(() => {
+    //call api to fetch user   info using this id and navigate accordingly.
+    const options = {
+      method: "GET",
+      url: `${baseUrl}/api/user/get-user/${id}`,
+    };
+
+    axios.request(options).then((response) => {
+      const { status, data } = response;
+      if (status == 200) {
+        setAuth({
+          ...auth,
+          user: data.data,
+        });
+        window.localStorage.setItem("auth", JSON.stringify(data));
+        navigate("/dashboard");
+      }
+    });
+  }, []);
 };
 
 export default Helper;
