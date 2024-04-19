53446fcea8e66b8756f02bb1575f2be3a44c3c5a
Md.Shibly Rahman Alve <alverohman2000@gmail.com>
2024-04-19 16:05:42 +0600

    minor change

--- src/pages/TaskAssignPage.jsx
+++ src/pages/TaskAssignPage.jsx
@@ -1,48 +1,59 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Drawer } from "rsuite";
-import CardSection, { AddCardSection } from "../components/CardSection";
+import CardSection from "../components/CardSection";
 import NavBar from "../components/NavBar";
 import Sidebar from "../components/Sidebar.jsx";
 import SlidePane from "../components/SlidePane.jsx";
 import { useSection } from "../context/SectionProvider.jsx";
-import { taskCardData } from "../data/data.js";
 
 const TaskAssignPage = () => {
-	const [isOpenPane, setIsOpenPane] = useState(false);
-	const { sectionData } = useSection();
-	console.log(sectionData);
-	const sections = ["1st Year", "2nd Year", "3rd Year", "4th Year"];
-	return (
-		<>
-			<div className="bg-blue-950 ">
-				<Sidebar>
-					<div className=" bg-white rounded-s-badge h-screen min-h-screen overflow-hidden">
-						<NavBar />
-						<div className=" flex px-3 w-full overflow-x-auto no-scrollbar">
-							<Drawer
-								open={isOpenPane}
-								onClose={() => setIsOpenPane(false)}
-								size="md"
-							>
-								<SlidePane />
-							</Drawer>
-							{sections?.map((section) => (
-								<CardSection
-									key={section}
-									SectionName={section}
-									TaskCardsData={taskCardData}
-									clickHandler={() => {
-										setIsOpenPane(true);
-									}}
-								/>
-							))}
-						</div>
-					</div>
-				</Sidebar>
-			</div>
-			<footer></footer>
-		</>
-	);
+  const [isOpenPane, setIsOpenPane] = useState(false);
+  const { sectionData } = useSection();
+  const [taskCardData1, setTaskCardData1] = useState([]);
+  const [taskCardData2, setTaskCardData2] = useState([]);
+  const [taskCardData3, setTaskCardData3] = useState([]);
+  const [taskCardData4, setTaskCardData4] = useState([]);
+  console.log(sectionData);
+  const sections = ["1st Year", "2nd Year", "3rd Year", "4th Year"];
+  return (
+    <>
+      <div className="bg-blue-950 ">
+        <Sidebar>
+          <div className=" bg-white rounded-s-badge h-screen min-h-screen overflow-hidden">
+            <NavBar />
+            <div className=" flex px-3 w-full overflow-x-auto no-scrollbar">
+              <Drawer
+                open={isOpenPane}
+                onClose={() => setIsOpenPane(false)}
+                size="md"
+              >
+                <SlidePane />
+              </Drawer>
+              {sections.map((section, index) => (
+                <CardSection
+                  key={section}
+                  SectionName={section}
+                  TaskCardsData={
+                    index === 0
+                      ? taskCardData1
+                      : index === 1
+                      ? taskCardData2
+                      : index === 2
+                      ? taskCardData3
+                      : taskCardData4
+                  }
+                  clickHandler={() => {
+                    setIsOpenPane(true);
+                  }}
+                />
+              ))}
+            </div>
+          </div>
+        </Sidebar>
+      </div>
+      <footer></footer>
+    </>
+  );
 };
 
 export default TaskAssignPage;
