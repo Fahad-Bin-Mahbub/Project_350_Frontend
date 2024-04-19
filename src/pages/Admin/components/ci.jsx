af28bd2d39d17eebc7d23f3c68a8b2a885d3565c
Md.Shibly Rahman Alve <alverohman2000@gmail.com>
2024-04-19 09:44:37 +0600

    ci integrated

--- src/pages/Admin/components/ci.jsx
+++ src/pages/Admin/components/ci.jsx
@@ -1,112 +1,149 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 import { BiSearch } from "react-icons/bi";
 import { LiaEditSolid } from "react-icons/lia";
 import { MdDeleteOutline } from "react-icons/md";
 import AddCiModal from "../../../components/admin modals/AddCiModal";
-const Ci = () => {
-	const columns = [
-		{
-			name: "Session",
-			selector: (row) => row.courseCode,
-		},
-		{
-			name: "Name",
-			selector: (row) => row.assignee,
-		},
-		{
-			name: "Email Address",
-			selector: (row) => row.email,
-		},
-		{
-			name: "Department",
-			selector: (row) => row.department,
-		},
-		{
-			name: "Action",
-			selector: (row) => {
-				return (
-					<div className="flex">
-						<LiaEditSolid size={20} />
-						<MdDeleteOutline size={20} />
-					</div>
-				);
-			},
-		},
-	];
+import { BASE_URL } from "../../../data/data";
 
-	const data = [
-		{
-			courseCode: "2024-1",
-			assignee: "Enam Hossain",
-			email: "enamhossain@student.sust.edu",
-			department: "CSE",
-			action: "View",
-		},
-		{
-			courseCode: "2023-2",
-			assignee: "Enam Hossain",
-			email: "enamhossain@student.sust.edu",
-			department: "CSE",
-			action: "View",
-		},
-		{
-			courseCode: "2024-1",
-			assignee: "Enam Hossain",
-			email: "enamhossain@student.sust.edu",
-			department: "CSE",
-			action: "View",
-		},
-	];
-	return (
-		<div className="w-full p-10 flex flex-col gap-4">
-			<div className="flex items-center justify-between p-3">
-				<div>
-					<div className="font-bold font-inter text-black text-[20px]">
-						Manage Chief Invigilator
-					</div>
-					<div className="text-black text-sm">
-						Here is a list of all Chief Invigilator
-					</div>
-				</div>
-				<div className="flex gap-4 items-center flex-wrap">
-					<div className="pt-2 relative mx-auto text-gray-600">
-						<input
-							className="border-2 border-gray-300 bg-white h-10 p-3 rounded-full text-sm focus:outline-none"
-							type="search"
-							name="search"
-							placeholder="Search"
-						/>
-						<BiSearch className="absolute right-0 top-0 mt-5 mr-4" />
-					</div>
-					{/* <Button className="w-44" title="Add Department" /> */}
-					{/* <AddDepartmentModal></AddDepartmentModal> */}
-					<AddCiModal />
-				</div>
-			</div>
-			<DataTable
-				columns={columns}
-				className=" min-h-fit-content"
-				customStyles={{
-					rows: {
-						style: {
-							position: "relative",
-							paddingTop: "20px",
-							paddingBottom: "20px",
-						},
-					},
-					headRow: {
-						style: {
-							backgroundColor: "#E3E9ED",
-							color: "#2F4551",
-						},
-					},
-				}}
-				data={data}
-				selectableRows
-				// pagination
-			/>
-		</div>
-	);
+const Ci = () => {
+  const baseUrl = BASE_URL;
+  const columns = [
+    {
+      name: "Session",
+      selector: (row) => row.courseCode,
+    },
+    {
+      name: "Name",
+      selector: (row) => row.assignee,
+    },
+    {
+      name: "Email Address",
+      selector: (row) => row.email,
+    },
+    {
+      name: "Department",
+      selector: (row) => row.department,
+    },
+    {
+      name: "Action",
+      selector: (row) => {
+        return (
+          <div className="flex">
+            <LiaEditSolid size={20} />
+            <MdDeleteOutline size={20} />
+          </div>
+        );
+      },
+    },
+  ];
+
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/api/head/get-all-ci`, {
+          withCredentials: true,
+        });
+        const { status, data } = response;
+
+        if (status === 200) {
+          const newData = [];
+          for (const item of data.data) {
+            let name = "";
+            let email = "";
+            let department = "";
+            const userId = item.user;
+
+            const response1 = await axios.get(
+              `${baseUrl}/api/user/get-user/${userId}`
+            );
+            const userData = response1.data.data;
+
+            name = `${userData.firstName} ${userData.lastName}`;
+            email = userData.email;
+            const deptId = userData.department;
+
+            const response2 = await axios.get(
+              `${baseUrl}/api/head/get-dept/${deptId}`,
+              { withCredentials: true }
+            );
+            const departmentData = response2.data.data;
+
+            department = departmentData.name;
+
+            newData.push({
+              courseCode: item.session,
+              assignee: name,
+              email,
+              department,
+              action: "View",
+            });
+          }
+          setTableData(newData);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(tableData);
+
+  return (
+    <div className="w-full p-10 flex flex-col gap-4">
+      <div className="flex items-center justify-between p-3">
+        <div>
+          <div className="font-bold font-inter text-black text-[20px]">
+            Manage Chief Invigilator
+          </div>
+          <div className="text-black text-sm">
+            Here is a list of all Chief Invigilator
+          </div>
+        </div>
+        <div className="flex gap-4 items-center flex-wrap">
+          <div className="pt-2 relative mx-auto text-gray-600">
+            <input
+              className="border-2 border-gray-300 bg-white h-10 p-3 rounded-full text-sm focus:outline-none"
+              type="search"
+              name="search"
+              placeholder="Search"
+            />
+            <BiSearch className="absolute right-0 top-0 mt-5 mr-4" />
+          </div>
+          {/* <Button className="w-44" title="Add Department" /> */}
+          {/* <AddDepartmentModal></AddDepartmentModal> */}
+          <AddCiModal />
+        </div>
+      </div>
+      <DataTable
+        columns={columns}
+        className=" min-h-fit-content"
+        customStyles={{
+          rows: {
+            style: {
+              position: "relative",
+              paddingTop: "20px",
+              paddingBottom: "20px",
+            },
+          },
+          headRow: {
+            style: {
+              backgroundColor: "#E3E9ED",
+              color: "#2F4551",
+            },
+          },
+        }}
+        data={tableData}
+        selectableRows
+        // pagination
+      />
+    </div>
+  );
 };
 
 export default Ci;
